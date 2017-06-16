75658790ab3ebe97170243ba6e8fda484febecad
mbutler <matthewtbutler@gmail.com>
2017-06-15 22:43:32 -0500

    working camera follow

--- js/game/states/game.js
+++ js/game/states/game.js
@@ -15,8 +15,8 @@ game.create = function () {
   background = map.createLayer('Background')
   top = map.createLayer('Top')    
   sprite1 = game.add.sprite(40, 100, 'characters')
-  foreground = map.createLayer('Foreground') 
-  
+  foreground = map.createLayer('Foreground')
+
   sprite1.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 60, true)
   sprite1.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 60, true)
   sprite1.animations.add('down', [18, 19, 20, 21, 22, 23, 24, 25, 26], 60, true)
@@ -26,38 +26,31 @@ game.create = function () {
 game.update = function(){
   // Check key states every frame.
   // Move ONLY one of the left and right key is hold.
+  this.camera.follow(sprite1, Phaser.Camera.FOLLOW_LOCKON); 
+
 
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
     sprite1.animations.play('left', true);
       sprite1.x -= speed;
-      console.log(sprite1.x)
-      //sprite1.angle = -15;
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
       sprite1.animations.play('right', true);
       sprite1.x += speed;
-      console.log(sprite1.x)
-
-      //sprite1.angle = 15;
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
     sprite1.animations.play('down', true);
       sprite1.y += speed;
-      console.log(sprite1.x)
-
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
     sprite1.animations.play('up', true);
       sprite1.y -= speed;
-      console.log(sprite1.x)
-
   }
   else
   {
       sprite1.animations.stop()
   }
-
 }
+
 
 
 module.exports = game;
--- js/game/states/preloader.js
+++ js/game/states/preloader.js
@@ -1,6 +1,8 @@
 var preloader = {};
 
 preloader.preload = function () {
+  this.game.world.setBounds(0, 0)
+
   this.game.load.spritesheet('characters', 'images/vx_characters.png', 64, 64, 32);
   this.game.load.tilemap('map', 'images/map.json', null, Phaser.Tilemap.TILED_JSON);
   this.game.load.image('grass', 'images/grass-tiles-2-small.png')
