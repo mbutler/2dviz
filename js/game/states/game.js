c82a386cf91a1f4eabed991a606cdd52b01fd11e
mbutler <matthewtbutler@gmail.com>
2017-06-15 21:58:12 -0500

    working tilemap

--- js/game/states/game.js
+++ js/game/states/game.js
@@ -1,9 +1,22 @@
 var game = {};
 var sprite1;
 var speed = 4;
+var map, background, foreground, top
 
 game.create = function () {
-  sprite1 = game.add.sprite(40, 100, 'characters');
+  map = game.add.tilemap('map')
+  
+  //the first parameter is the name given in Tiled, second is the name given in preloader.js
+  map.addTilesetImage('grass-tiles-2-small', 'grass');
+  map.addTilesetImage('tree2-final', 'tree');
+  
+  //need to refer to these layers by the layer name in Tiled
+  //add the character before the Foreground layer to make it look like he's walking behind
+  background = map.createLayer('Background')
+  top = map.createLayer('Top')    
+  sprite1 = game.add.sprite(40, 100, 'characters')
+  foreground = map.createLayer('Foreground') 
+  
   sprite1.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 60, true)
   sprite1.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 60, true)
   sprite1.animations.add('down', [18, 19, 20, 21, 22, 23, 24, 25, 26], 60, true)
--- js/game/states/preloader.js
+++ js/game/states/preloader.js
@@ -2,6 +2,9 @@ var preloader = {};
 
 preloader.preload = function () {
   this.game.load.spritesheet('characters', 'images/vx_characters.png', 64, 64, 32);
+  this.game.load.tilemap('map', 'images/map.json', null, Phaser.Tilemap.TILED_JSON);
+  this.game.load.image('grass', 'images/grass-tiles-2-small.png')
+  this.game.load.image('tree', 'images/tree2-final.png')
 };
 
 preloader.create = function () {
