9071607c2b745519001bd149ebd2924bf2aca755
mbutler <matthewtbutler@gmail.com>
2017-06-16 08:37:08 -0500

    set world boundaries and collision using arcade physics engine

--- js/game/states/game.js
+++ js/game/states/game.js
@@ -1,56 +1,62 @@
-var game = {};
-var sprite1;
-var speed = 4;
+var game = {}
+var character
+var speed = 4
 var map, background, foreground, top
 
 game.create = function () {
+
+  //add the map and set bounds. Turn on arcade physics so we can collide with boundaries
   map = game.add.tilemap('map')
+  game.world.setBounds(0, 0, 3200, 3200)
+  game.physics.startSystem(Phaser.Physics.ARCADE)
   
-  //the first parameter is the name given in Tiled, second is the name given in preloader.js
-  map.addTilesetImage('grass-tiles-2-small', 'grass');
-  map.addTilesetImage('tree2-final', 'tree');
+  //the first parameter is the name given in Tiled, second is the chache name given in preloader.js
+  map.addTilesetImage('grass-tiles-2-small', 'grass')
+  map.addTilesetImage('tree2-final', 'tree')
   
   //need to refer to these layers by the layer name in Tiled
   //add the character before the Foreground layer to make it look like he's walking behind
   background = map.createLayer('Background')
   top = map.createLayer('Top')    
-  sprite1 = game.add.sprite(40, 100, 'characters')
+  character = game.add.sprite(40, 100, 'characters')
   foreground = map.createLayer('Foreground')
 
-  sprite1.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 60, true)
-  sprite1.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 60, true)
-  sprite1.animations.add('down', [18, 19, 20, 21, 22, 23, 24, 25, 26], 60, true)
-  sprite1.animations.add('up', [0, 1, 2, 3, 4, 5, 6, 7, 8], 60, true)
-};
+  //enable the physics engine only for sprites that need them
+  game.physics.arcade.enable(character, true)
+  character.body.collideWorldBounds = true
+
+  character.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 60, true)
+  character.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 60, true)
+  character.animations.add('down', [18, 19, 20, 21, 22, 23, 24, 25, 26], 60, true)
+  character.animations.add('up', [0, 1, 2, 3, 4, 5, 6, 7, 8], 60, true)
+}
 
 game.update = function(){
+  //lock the camera on our sprite guy and follow
+  this.camera.follow(character, Phaser.Camera.FOLLOW_LOCKON) 
+
   // Check key states every frame.
-  // Move ONLY one of the left and right key is hold.
-  this.camera.follow(sprite1, Phaser.Camera.FOLLOW_LOCKON); 
-
-
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    sprite1.animations.play('left', true);
-      sprite1.x -= speed;
+      character.animations.play('left', true)
+      character.x -= speed
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-      sprite1.animations.play('right', true);
-      sprite1.x += speed;
+      character.animations.play('right', true)
+      character.x += speed
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    sprite1.animations.play('down', true);
-      sprite1.y += speed;
+      character.animations.play('down', true)
+      character.y += speed
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    sprite1.animations.play('up', true);
-      sprite1.y -= speed;
+      character.animations.play('up', true)
+      character.y -= speed
   }
-  else
-  {
-      sprite1.animations.stop()
+  else {
+      character.animations.stop()
   }
 }
 
 
 
-module.exports = game;
+module.exports = game
--- js/game/states/preloader.js
+++ js/game/states/preloader.js
@@ -1,16 +1,14 @@
-var preloader = {};
+var preloader = {}
 
-preloader.preload = function () {
-  this.game.world.setBounds(0, 0)
-
-  this.game.load.spritesheet('characters', 'images/vx_characters.png', 64, 64, 32);
-  this.game.load.tilemap('map', 'images/map.json', null, Phaser.Tilemap.TILED_JSON);
+preloader.preload = function () {  
+  this.game.load.spritesheet('characters', 'images/vx_characters.png', 64, 64, 32)
+  this.game.load.tilemap('map', 'images/map.json', null, Phaser.Tilemap.TILED_JSON)
   this.game.load.image('grass', 'images/grass-tiles-2-small.png')
   this.game.load.image('tree', 'images/tree2-final.png')
-};
+}
 
 preloader.create = function () {
-  this.game.state.start('game');
-};
+  this.game.state.start('game')
+}
 
-module.exports = preloader;
+module.exports = preloader
