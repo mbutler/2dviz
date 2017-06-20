4fb94ba9e5d7fc76050da2129ae931b2cce1c27f
mbutler <matthewtbutler@gmail.com>
2017-06-19 20:10:46 -0500

    working collision layer
    set character sprite to have a body velocity so it can interact with the physics engine

--- js/game/states/game.js
+++ js/game/states/game.js
@@ -1,62 +1,70 @@
 var game = {}
-var character
-var speed = 4
-var map, background, foreground, top
+var speed = 150
+var map, background, foreground, top, blocked, character
 
 game.create = function () {
+  game.physics.startSystem(Phaser.Physics.ARCADE)
 
   //add the map and set bounds. Turn on arcade physics so we can collide with boundaries
   map = game.add.tilemap('map')
-  game.world.setBounds(0, 0, 3200, 3200)
-  game.physics.startSystem(Phaser.Physics.ARCADE)
+  game.world.setBounds(0, 0, 3200, 3200)  
   
-  //the first parameter is the name given in Tiled, second is the chache name given in preloader.js
+  //the first parameter is the name given in Tiled, second is the cache name given in preloader.js
   map.addTilesetImage('grass-tiles-2-small', 'grass')
   map.addTilesetImage('tree2-final', 'tree')
   
   //need to refer to these layers by the layer name in Tiled
   //add the character before the Foreground layer to make it look like he's walking behind
   background = map.createLayer('Background')
+  blocked = map.createLayer('Blocked')
   top = map.createLayer('Top')    
   character = game.add.sprite(40, 100, 'characters')
   foreground = map.createLayer('Foreground')
+
+  map.setCollisionBetween(1, 100, true, 'Blocked')
 
   //enable the physics engine only for sprites that need them
   game.physics.arcade.enable(character, true)
   character.body.collideWorldBounds = true
 
-  character.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 60, true)
-  character.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 60, true)
-  character.animations.add('down', [18, 19, 20, 21, 22, 23, 24, 25, 26], 60, true)
-  character.animations.add('up', [0, 1, 2, 3, 4, 5, 6, 7, 8], 60, true)
+  character.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 20, true)
+  character.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 20, true)
+  character.animations.add('down', [18, 19, 20, 21, 22, 23, 24, 25, 26], 20, true)
+  character.animations.add('up', [0, 1, 2, 3, 4, 5, 6, 7, 8], 20, true)
 }
 
 game.update = function(){
+  game.physics.arcade.collide(character, blocked)
+
   //lock the camera on our sprite guy and follow
-  this.camera.follow(character, Phaser.Camera.FOLLOW_LOCKON) 
+  this.camera.follow(character, Phaser.Camera.FOLLOW_LOCKON)
+
+  character.body.velocity.x = 0
+  character.body.velocity.y = 0 
 
   // Check key states every frame.
+  // Sprites need a velocity to work with physics
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
       character.animations.play('left', true)
-      character.x -= speed
+      character.body.velocity.x -= speed
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
       character.animations.play('right', true)
-      character.x += speed
+      character.body.velocity.x += speed
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
       character.animations.play('down', true)
-      character.y += speed
+      character.body.velocity.y += speed
   }
   else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
       character.animations.play('up', true)
-      character.y -= speed
+      character.body.velocity.y -= speed
   }
   else {
       character.animations.stop()
+      character.body.velocity.x = 0
+      character.body.velocity.y = 0
   }
 }
-
-
 
 module.exports = game
